38870f5d90aa09dd0b11d3a883d88c41f73b545e
dpami507 <amidon.david@comcast.net>
2026-09-10 18:25:59 -0400

    Started Work on John Conway
    it doesn't work :(

--- apps/life/rules/JohnConway.cpp
+++ apps/life/rules/JohnConway.cpp
@@ -24,7 +24,14 @@ class Underpopulation : public Condition {
 public:
   bool Test(const AgentContext& context) override {
     // todo: implement the underpopulation condition
-    throw std::logic_error("Underpopulation condition not implemented yet");
+    if (!context.isAlive)
+      return context.isAlive;
+
+    if (context.aliveNeighbors <= 1)
+    {
+        return false;
+    }
+    return context.isAlive;
   }
 };
 
@@ -32,7 +39,14 @@ class Overpopulation : public Condition {
 public:
   bool Test(const AgentContext& context) override {
     // todo: implement the overpopulation condition
-    throw std::logic_error("Overpopulation condition not implemented yet");
+    if (!context.isAlive)
+        return context.isAlive;
+
+    if (context.aliveNeighbors >= 4)
+    {
+        return false;
+    }
+    return context.isAlive;
   }
 };
 
@@ -40,7 +54,14 @@ class Reproduction : public Condition {
 public:
   bool Test(const AgentContext& context) override {
     // todo: implement the reproduction condition
-    throw std::logic_error("Reproduction condition not implemented yet");
+    if (context.isAlive)
+        return context.isAlive;
+
+    if (context.aliveNeighbors >= 3)
+    {
+        return true;
+    }
+    return context.isAlive;
   }
 };
 
@@ -51,7 +72,8 @@ public:
     // hint:
     //   use the context.world.SetNext() to set the next state of the cell to dead
     //   use the context.position to get the current cell's position
-    throw std::logic_error("Die action not implemented yet");
+
+    context.world.SetNext(context.position, !context.isAlive);
   }
 };
 
@@ -59,7 +81,7 @@ class BornAction : public Action {
 public:
   void Execute(const AgentContext& context) override {
     // see hints in DieAction
-    throw std::logic_error("Born action not implemented yet");
+      context.world.SetNext(context.position, !context.isAlive);
   }
 };
 
@@ -67,7 +89,7 @@ class StayAliveAction : public Action {
 public:
   void Execute(const AgentContext& context) override {
     // see hints in DieAction
-    throw std::logic_error("StayAlive action not implemented yet");
+      context.world.SetNext(context.position, context.isAlive);
   }
 };
 
@@ -75,7 +97,7 @@ class StayDeadAction : public Action {
 public:
   void Execute(const AgentContext& context) override {
     // see hints in DieAction
-    throw std::logic_error("StayDead action not implemented yet");
+      context.world.SetNext(context.position, context.isAlive);
   }
 };
 }  // namespace conway
@@ -92,10 +114,13 @@ JohnConway::JohnConway() {
   const auto born = std::make_shared<BornAction>();
 
   // todo: add transitions and actions for alive, dead. example:
-  //   alive->AddTransition(std::make_shared<Underpopulation>(), dead, {die});
-  //   dead->AddAction(std::make_shared<StayDeadAction>());
+  dead->AddAction(std::make_shared<StayDeadAction>());
+  alive->AddAction(std::make_shared<StayAliveAction>());
 
   // begin solution
+  alive->AddTransition(std::make_shared<Underpopulation>(), dead, {die});
+  alive->AddTransition(std::make_shared<Overpopulation>(), dead, {die});
+  dead->AddTransition(std::make_shared<Reproduction>(), alive, {born});
   // note: log instead of throw - the constructor runs at app startup and at
   // every fixture load; throwing here would kill the process before it runs.
   SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "JohnConway: transitions and actions for alive and dead states not implemented yet");
@@ -133,7 +158,26 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   //   world.Get({point.x + dx, point.y + dy}) wraps around the borders (toroidal)
   // begin solution
 
-  throw std::logic_error("CountNeighbors not implemented yet");
+    int minX = point.x - 1;
+    int maxX = point.x + 1;
+
+    int minY = point.y - 1;
+    int maxY = point.y + 1;
+
+    int count = 0;
+    for (int dx = minX; dx <= maxX; dx++)
+    {
+        for (int dy = minY; dy <= maxY; dy++)
+        {
+            if (dx == 0 && dy == 0)
+                continue;
+
+            if (world.Get(Point2D(dx, dy)))
+                count++;
+        }
+    }
+
+    return count;
 
   // end solution
 }
